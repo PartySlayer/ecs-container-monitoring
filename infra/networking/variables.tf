6bd8492eee3e55909e5d00e92cda540aa2b19a80
PartySlayer <squadrittichristian@gmail.com>
2026-02-27 01:15:44 +0100

    ++ modulo networking

---
+++ infra/networking/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  description = "Nome del progetto (usato nei prefissi)"
+  default     = "serverless-observability-ecs-0x0"
+}
+
+variable "environment" {
+  description = "L'ambiente a cui sono riferite le risorse all'interno dello stato remoto"
+  default     = "sviluppo"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "Public subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "Private subnet CIDRs"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "azs" {
+  description = "Availability Zones"
+  type        = list(string)
+  default     = ["eu-west-1a", "eu-west-1b"]
+}
