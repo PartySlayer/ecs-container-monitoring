20420736d43f03d3f72e40bed1a0f78452a545c2
PartySlayer <squadrittichristian@gmail.com>
2026-02-27 22:18:32 +0100

    modificate refs repo nel ruolo per pipeline

--- 00-bootstrap-state/main.tf
+++ 00-bootstrap-state/main.tf
@@ -87,7 +87,7 @@ data "aws_iam_policy_document" "github_trust_policy" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:PartySlayer/serverless-container-monitoring"]
+      values   = ["repo:PartySlayer/ecs-container-monitoring:*"]
     }
   }
 }
